d81e9be46a08aecf6f6d7f3ba003c907c305270a
ChaitanyaV98 <chaitanya@betalectic.com>
2025-08-21 15:46:22 +0530

    added where based filter query and sort users column asc or dsc

---
+++ src/concepts/filtering-sorting.js
@@ -0,0 +1,25 @@
+import query from "../db/db-connection.js";
+
+//where clause
+export async function getUsersWhere(condition) {
+  const getUsersQuery = `SELECT * FROM users
+    WHERE ${condition}`;
+  try {
+    const res = await query(getUsersQuery);
+    return res.rows;
+  } catch (e) {
+    console.log("error occured while applying where condition");
+  }
+}
+
+export async function getSortedUsers(column, order = "ASC") {
+  const sortColumn = `SELECT * FROM users
+    ORDER BY ${column} ${order} `;
+
+  try {
+    const res = await query(sortColumn);
+    return res.rows;
+  } catch (error) {
+    console.log("error while applying sort condition", error);
+  }
+}
